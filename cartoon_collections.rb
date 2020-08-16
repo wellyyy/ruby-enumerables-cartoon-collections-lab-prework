941a73616e6d83f33af89816334a1d219779cd45
Wesley Drake Faircloth <wesfairclothh@gmail.com>
2020-08-16 00:11:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,12 +7,9 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.each do |n|
-    if n.length > 4 
-      true
-    end
+  array.any? do |n|
+    
   end
-  false
 end
 
 def find_the_cheese# code an argument here
