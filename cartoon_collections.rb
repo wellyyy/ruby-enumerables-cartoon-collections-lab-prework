94c19baf16b3cb396109a2e3cd7e981c7b12c248
Wesley Drake Faircloth <wesfairclothh@gmail.com>
2020-08-16 00:08:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,12 +3,14 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  #array.map { |string| string.upcase + "!"}
   array.map { |string| string.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each do |n|
+    if n.length > 4 
+  end
+  
 end
 
 def find_the_cheese# code an argument here
